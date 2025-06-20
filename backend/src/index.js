2ae54f66414e4ea32e1db23140329c6d33eed1eb
dhanyabad11 <dhanyabadbehera@gmail.com>
2025-06-20 22:41:39 +0530

    add basic routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import apiRouter from "./routes/index.js";
 
 import { PORT } from "./config/serverConfig.js";
 
@@ -7,6 +8,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
+app.use("/api", apiRouter);
+
+app.get("/ping", (req, res) => {
+    return res.json({ message: "pong" });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
