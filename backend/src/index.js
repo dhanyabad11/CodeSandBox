dc917231bfc07af5f900d2f42007ecb6335ba3e6
dhanyabad11 <dhanyabadbehera@gmail.com>
2025-07-24 22:49:36 +0530

    add chokidar

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import apiRouter from "./routes/index.js";
 import { Server } from "socket.io";
 
 import { PORT } from "./config/serverConfig.js";
+import chokidar from "chokidar";
+import path from "path";
 
 const app = express();
 const server = createServer(app);
@@ -27,6 +29,35 @@ app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
     return res.json({ message: "pong" });
+});
+
+const editorNamespace = io.of("/editor");
+
+editorNamespace.on("connection", (socket) => {
+    console.log("editor connected");
+
+    let projectId = "123";
+
+    if (projectId) {
+        const watcher = chokidar.watch(`./project/${projectId}`, {
+            ignored: (path) => path.includes("node_modules"),
+            persistent: true,
+
+            awaitWriteFinish: {
+                stabilityThreshold: 2000,
+            },
+            ignoreInitial: true,
+        });
+
+        watcher.on("all", (event, path) => {
+            console.log(event, path);
+            socket.emit("filechange", { path });
+        });
+    }
+
+    socket.on("message", () => {
+        console.log("got a message event", data);
+    });
 });
 
 server.listen(PORT, () => {
